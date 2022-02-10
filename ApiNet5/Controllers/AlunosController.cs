b40ca66a652ea079e347ea6554d25aa687080d80
rleme <rodrigocleme@gmail.com>
2022-02-10 16:21:41 -0300

    Teste01

---
+++ ApiNet5/Controllers/AlunosController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ApiNet5.Models;
+
+namespace ApiNet5.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlunosController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public AlunosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Alunoes
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Aluno>>> GetAlunos()
+        {
+            return await _context.Alunos.ToListAsync();
+        }
+
+        // GET: api/Alunoes/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Aluno>> GetAluno(int id)
+        {
+            var aluno = await _context.Alunos.FindAsync(id);
+
+            if (aluno == null)
+            {
+                return NotFound();
+            }
+
+            return aluno;
+        }
+
+        // PUT: api/Alunoes/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAluno(int id, Aluno aluno)
+        {
+            if (id != aluno.AlunoId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(aluno).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AlunoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Alunoes
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Aluno>> PostAluno(Aluno aluno)
+        {
+            _context.Alunos.Add(aluno);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetAluno", new { id = aluno.AlunoId }, aluno);
+        }
+
+        // DELETE: api/Alunoes/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAluno(int id)
+        {
+            var aluno = await _context.Alunos.FindAsync(id);
+            if (aluno == null)
+            {
+                return NotFound();
+            }
+
+            _context.Alunos.Remove(aluno);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool AlunoExists(int id)
+        {
+            return _context.Alunos.Any(e => e.AlunoId == id);
+        }
+    }
+}
---
+++ ApiNet5/Models/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+namespace ApiNet5.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Aluno> Alunos { get; set; }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+    }
+}
